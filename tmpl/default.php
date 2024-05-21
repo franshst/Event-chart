697395f8e9f998f4d08545d9b2a54786ebba071b
franshst <129629223+franshst@users.noreply.github.com>
2024-05-21 21:39:39 +0200

    Primitief tekst filter

--- tmpl/default.php
+++ tmpl/default.php
@@ -26,11 +26,15 @@ echo '</script>';
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js">
 </script>
 
+<div id="chartFilters">
+    <h3>Filters</h3>
+    <input type="text" name="Event" value="" onchange="changeTitle(this.value)">
+</div>
 <canvas id="myChart" width="800" height="600"></canvas>
 <script>
 
-    //document.write(JSON.stringify(chartData));
     // function convertDates written by chatgpt
+    // conversion of PHP datetime values to javascript datetime values
     function convertDates(obj, dateFields) {
         if (Array.isArray(obj)) {
             obj.forEach(item => {
@@ -46,7 +50,8 @@ echo '</script>';
             }
         }
     }
-    /*
+
+    // matches true, if any member in an array is equal to any member in other array
     function anyMatch(array1,array2) {
         var match = false;
         main: for(var element1 in array1) {
@@ -58,51 +63,55 @@ echo '</script>';
         }
         return match;
     }
-    */
-//      console.log(JSON.stringify(chartData));
+
+    // populate the chartjs datasets, from chartData, with filters
+    function loadData(chartData,datasets,from,to,title,category) {
+        for (var event_id in chartData) {
+    //            console.log(chartData[event_id].title);
+
+            if (chartData[event_id].event_date >= from && 
+                chartData[event_id].event_date <= to &&
+                (title === '' || chartData[event_id].title.includes(title))
+//                (category.length == 0 || anyMatch(chartData[event_id].category, category))
+                ){
+                console.log("Filtered events: " + chartData[event_id].title);
+                datasets.push({
+                    label: chartData[event_id].title,
+                    order: 0-chartData[event_id].event_date,
+                    data: chartData[event_id].sales.map((row) => ({
+                        x: row.days_before_event,
+                        y: row.cum_tickets_sold
+                    })),
+                    borderColor: '#' + Math.floor(Math.random()*16777215).toString(16), // Random color
+                    fill: false
+                });
+            }
+        }
+    }
+
+    // convert PHP data to javascript data
     convertDates(chartData,['event_date','sale_date']);
-//      console.log(JSON.stringify(chartData));
 
-//  Calculate filters
-
+    //  Calculate filters
     pastFilter = 365;
     rangeFilter = 30;
     // include events in this date range
     var fromDate = new Date(); fromDate.setDate(fromDate.getDate() - pastFilter);
     var toDate = new Date(); toDate.setDate(toDate.getDate() + rangeFilter);
-    document.write(fromDate);
-//    var titleFilter = 'Mezrab';
-//    var categoryFilter = ['Bal'];
+    // document.write(fromDate);
+    var titleFilter = 'Mezrab';
+    var categoryFilter = ['Bal'];
 
-//    console.log(fromDate);
-//    console.log(toDate);
 
-    var ctx = document.getElementById('myChart').getContext('2d');
+    // load data into chart
     var datasets = [];
-    for (var event_id in chartData) {
-            console.log (chartData[event_id].title + '<br>');
-
-        if (chartData[event_id].event_date >= fromDate && 
-            chartData[event_id].event_date <= toDate
-//            (titleFilter === '' || chartData[event_id].title.includes(titleFilter)) &&
-//            (categoryFilter.length == 0 || anyMatch(chartData[event_id].category, categoryFilter))
-            ){
-            datasets.push({
-                label: chartData[event_id].title,
-                order: 0-chartData[event_id].event_date,
-                data: chartData[event_id].sales.map((row) => ({
-                    x: row.days_before_event,
-                    y: row.cum_tickets_sold
-                })),
-                borderColor: '#' + Math.floor(Math.random()*16777215).toString(16), // Random color
-                fill: false
-            });
-        }
-    }
+    loadData(chartData, datasets, fromDate, toDate, titleFilter, categoryFilter);
 
 //        console.log(JSON.stringify(datasets));
 
 
+    // create chart
+    var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
         type: 'line',
         data: {
@@ -131,4 +140,16 @@ echo '</script>';
             }
         }
     });
+
+    // callback functions
+    // titlefilter changed
+    function changeTitle(newTitleFilter) {
+        console.log("New title filter: " + newTitleFilter);
+        titleFilter = newTitleFilter;
+        datasets = [];
+        loadData(chartData, datasets, fromDate, toDate, titleFilter, categoryFilter);
+        console.log("datasets: " + datasets.length);
+        chart.data.datasets = datasets;
+        chart.update();
+    }
 </script>
